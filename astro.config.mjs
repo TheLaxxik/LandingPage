276126a08ea923933baecce6c0b7cb4133894cfe
Ivan Dvorovy <ivandvorovy@gmail.com>
2026-05-30 16:03:48 +0200

    fix: Remove commented URL from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sanity from '@sanity/astro';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://taskflow.esticode.sk',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [
     sanity({
       projectId: 'jh80nfpn',
